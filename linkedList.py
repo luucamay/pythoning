6acb77460d2f966e4e541884f5ded47645478ec1
luucamay <lupemaydana@gmail.com>
2019-10-02 22:37:53 -0400

    created functions to print list and delete duplicates

--- linkedList.py
+++ linkedList.py
@@ -2,9 +2,37 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
+    
+    def traverse(self):
+        node = self
+        while node != None:
+            print node.data,
+            node = node.next
+        print ''    
+
+    def remove_duplicates(self):
+        no_dups = []
+        node = self
+        previous = None
+        while node != None:
+            if node.data in no_dups:
+                previous.next = node.next
+            else:
+                no_dups.append(node.data)
+                previous = node
+            node = node.next
+        print no_dups
 
 head = Node(8)
 head.next = Node(12)
-head.next.next = Node(8)
-print head.data
-print "holap"
+head.next.next = Node(13)
+head.next.next.next = Node (8)
+head.next.next.next.next = Node(7)
+head.next.next.next.next.next = Node (13)
+head.next.next.next.next.next.next = Node (13)
+head.next.next.next.next.next.next.next = Node (13)
+head.next.next.next.next.next.next.next.next = Node (11)
+
+head.traverse()
+head.remove_duplicates()
+head.traverse()
