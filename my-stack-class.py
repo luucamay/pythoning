c65f145a9e8adad857493d7a1b11f332d26181c3
luucamay <lupemaydana@gmail.com>
2019-10-04 11:08:02 -0400

    adding pop and push functions to the stack Class

--- my-stack-class.py
+++ my-stack-class.py
@@ -26,11 +26,17 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        if self.head:
+            new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        if self.head:
+            deleted_element = self.head
+            self.head = self.head.next
+            return deleted_element
+        return None
 
 class Stack(object):
     def __init__(self,top=None):
@@ -38,11 +44,11 @@ class Stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.ll.delete_first()
     
 # Test cases
 # Set up some Elements
