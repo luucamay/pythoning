8378c396e5b22c3937fcd740ba88ca7a8d77cd25
luucamay <lupemaydana@gmail.com>
2019-10-06 22:27:13 -0400

    implement insert method at binary search tree class

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -9,15 +9,33 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        pass
+        self.insert_helper(self.root, new_val)
 
     def search(self, find_val):
         return False
-
+        
+    def insert_helper(self, current, new_val):
+        if current:
+            if current.value < new_val:
+                if current.right:
+                    self.insert_helper(current.right, new_val)
+                else:
+                    current.right = Node(new_val)
+            else:
+                if current.left:
+                    self.insert_helper(current.left, new_val)
+                else:
+                    current.left = Node(new_val)
+        
     def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
         return self.inorder_print(self.root,"")[:-1]
 
     def inorder_print(self, currentNode, traversal):
+        """Helper method - use this to create a 
+        recursive print solution."""
         if currentNode:
             traversal = self.inorder_print(currentNode.left,traversal)
             traversal += (str(currentNode.value) + "-")
