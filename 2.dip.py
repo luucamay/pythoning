954b831e07294d883a714845cba332f588f9869e
luucamay <lupemaydana@gmail.com>
2019-10-08 16:38:12 -0400

    improved BFS algorithm for Daily Interview Problem

--- 2.dip.py
+++ 2.dip.py
@@ -6,16 +6,14 @@ class Node:
 
 def print_level_order(root):
     queue = []
-    while root:
+    queue.append(root)
+    while queue:
+        root = queue.pop(0)
         print root.val,
         if root.left:
             queue.append(root.left)
         if root.right:
             queue.append(root.right)
-        if queue:
-            root = queue.pop(0)
-        else:
-            root = None
 
 root = Node(1, Node(2), Node(3, Node(4), Node(5)))
 print_level_order(root)
