5404f9a0c28608f9d5638d65aa5d3e1dcd1f67d7
luucamay <lupemaydana@gmail.com>
2019-10-10 00:00:20 -0400

    added binary_search_function

--- binary_search.py
+++ binary_search.py
@@ -9,13 +9,27 @@ meaning there are no repeated values, and
 elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
-
+import math
 def binary_search(input_array, value):
-        """Your code goes here."""
-            return -1
+    size = len(input_array)
+    if size > 0:
+        number_of_loops = int(round(math.sqrt(size)))
+        print number_of_loops
+        size = size/2 + 1
+        index = size - 1
+        for i in range(number_of_loops):
+            print i, size, index
+            if input_array[index] == value:
+                return index
+            size = size/2
+            if input_array[index] > value:
+                index = index - size
+            else:
+                index = index + size
+    return -1
 
-        test_list = [1,3,9,11,15,19,29]
-        test_val1 = 25
-        test_val2 = 15
-        print binary_search(test_list, test_val1)
-        print binary_search(test_list, test_val2)
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print binary_search(test_list, test_val1)
+print binary_search(test_list, test_val2)
