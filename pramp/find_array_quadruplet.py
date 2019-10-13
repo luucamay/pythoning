f5f3de6b282304222d2f6c921c7bc2bdcd924be2
luucamay <lupemaydana@gmail.com>
2019-10-12 23:00:03 -0400

    Pramp interview practice - find array quadruplet solution

--- pramp/find_array_quadruplet.py
+++ pramp/find_array_quadruplet.py
@@ -1,22 +1,24 @@
 def find_array_quadruplet(arr, s):
-    n = len(arr)
-    if n < 4:
-        return []
-    arr.sort()
-    for i in range(n - 4):
-        for j in range(i+1, n - 3):
-            low = j + 1
-            high = n - 1
-            r = s - (arr[i] + arr[j])
-            while low < high:
-                aux = arr[low] + arr[high]
-                if r > aux:
-                    low += 1
-                elif r < aux:
-                    high -= 1
-                else:
-                    return [arr[i], arr[j], arr[low], arr[high]]
+  n = len(arr)
+  if n < 4:
     return []
+  arr.sort()
+  for i in range(n - 3):
+    for j in range(i + 1, n - 2):      
+      low = j + 1
+      high = n - 1
+      r = s - (arr[i] + arr[j])
+      while low < high:
+        aux = arr[low] + arr[high]
+        if r > aux:
+          low +=1
+        elif r < aux:
+          high -=1
+        else:
+          return [arr[i], arr[j], arr[low], arr[high]]
+  return []
 
-# Test
-print find_array_quadruplet([2,7,4,0,9,5,1,3], 20)
+# myarray = [2, 7, 4, 0, 9, 5, 1, 3]
+myarray = [4,4,4,4]
+s = 16
+print find_array_quadruplet(myarray, s)
