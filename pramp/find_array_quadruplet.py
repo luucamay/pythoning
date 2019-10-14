e7d7f54784867fcb0e29420190c89a6a29f8e851
luucamay <lupemaydana@gmail.com>
2019-10-13 22:56:08 -0400

    Added problem description

--- pramp/find_array_quadruplet.py
+++ pramp/find_array_quadruplet.py
@@ -1,3 +1,12 @@
+'''
+Given an unsorted array of integers arr and a number s, write a function findArrayQuadruplet
+that finds four numbers (quadruplet) in arr that sum up to s.
+Your function should return an array of these numbers in an ascending order.
+If such a quadruplet doesn’t exist, return an empty array.
+
+Note that there may be more than one quadruplet in arr whose sum is s.
+You’re asked to return the first one you encounter (considering the results are sorted).
+'''
 def find_array_quadruplet(arr, s):
   n = len(arr)
   if n < 4:
