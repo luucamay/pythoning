a261533800818a28454e15f7e69c19f790773954
luucamay <lupemaydana@gmail.com>
2019-10-23 10:36:49 -0400

    Solution to Min Stack problem, Cracking the coding interview

---
+++ stack_min.py
@@ -0,0 +1,37 @@
+import sys
+class NodeWithMin:
+    def __init__(self, v, mini):
+        self.value = v
+        self.min = mini
+
+class Stack:
+    def __init__(self):
+        self.storage = []
+    def push(self, value):
+        new_min = min(value, self.min_stack())
+        new_element = NodeWithMin(value, new_min)
+        self.storage.append(new_element)
+    def pop_stack(self):
+        return self.storage.pop()
+    def min_stack(self):
+        if self.storage:
+            return self.peek().min
+        return sys.maxsize
+    def peek(self):
+        return self.storage[-1]
+
+
+# Test, create stack of objects
+stack = Stack()
+stack.push(4)
+stack.push(2)
+stack.push(3)
+stack.push(1)
+element = stack.pop_stack()
+print element.min, element.value
+element = stack.pop_stack()
+print element.min, element.value
+element = stack.pop_stack()
+print element.min, element.value
+element = stack.pop_stack()
+print element.min, element.value
