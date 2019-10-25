b9669088dc2351d1b2a462d52ee4e0268c897d90
luucamay <lupemaydana@gmail.com>
2019-10-24 20:55:57 -0400

    BFS implemented for a graph with adjacent list

---
+++ route_between_nodes.py
@@ -0,0 +1,61 @@
+from collections import defaultdict 
+  
+# This class represents a directed graph 
+# using adjacency list representation 
+class Graph: 
+  
+    # Constructor 
+    def __init__(self): 
+  
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
+  
+    # function to add an edge to graph 
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    # Function to print a BFS of graph 
+    def BFS(self, s): 
+  
+        # Mark all the vertices as not visited 
+        visited = defaultdict(bool) # set() can be used instead 
+  
+        # Create a queue for BFS 
+        queue = [] 
+  
+        # Mark the source node as  
+        # visited and enqueue it 
+        queue.append(s) 
+        visited[s] = True
+  
+        while queue: 
+  
+            # Dequeue a vertex from  
+            # queue and print it 
+            s = queue.pop(0) 
+            print s,
+  
+            # Get all adjacent vertices of the 
+            # dequeued vertex s. If a adjacent 
+            # has not been visited, then mark it 
+            # visited and enqueue it 
+            for i in self.graph[s]: 
+                if not visited[i]: 
+                    queue.append(i) 
+                    visited[i] = True
+  
+# Driver code 
+  
+# Create a graph given in 
+# the above diagram 
+g = Graph() 
+g.addEdge(0, 1) 
+g.addEdge(0, 2) 
+g.addEdge(1, 2) 
+g.addEdge(2, 0) 
+g.addEdge(2, 4) 
+g.addEdge(4, 4) 
+  
+print ("Following is Breadth First Traversal"
+                  " (starting from vertex 2)") 
+g.BFS(2) 
