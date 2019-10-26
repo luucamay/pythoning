3afc5df5936707f3f356872e85acfc5587634c3e
luucamay <lupemaydana@gmail.com>
2019-10-26 16:42:44 -0400

    This solution doesn't work, but is part of my way of solving problems so Iwant to save the code here

--- cracking_the_coding_interview/sorted_merge.py
+++ cracking_the_coding_interview/sorted_merge.py
@@ -8,10 +8,12 @@ Hints
 '''
 # Solution 1: using extra memory
 def merge1(a, b):
+    sizeB = len(b)
+    sizeA = len(a) - sizeB
     new_array = []
     i = 0
     j = 0
-    while j < len(b) and i < len(a):
+    while j < len(b) and i < sizeA:
         if a[i] > b[j]:
             new_array.append(b[j])
             j += 1
@@ -23,6 +25,25 @@ def merge1(a, b):
     else:
         new_array.extend(b[j:])
     return new_array
-A = [10, 20, 25, 30, 32, 50]
+
+# Test
+A = [None]*12
+A[0], A[1], A[2], A[3], A[4], A[5] = 10, 20, 25, 30, 32, 50
+print A
 B = [5, 15, 16, 17, 40, 42]
-print merge1(A,B)
+# print merge1(A,B)
+
+# Solution 2: thanks to hint, start array a from back
+# This solution doesn't work because it is never assigning value at the end of the list
+def merge2(a, b):
+    sizeB = len(b)
+    sizeA = len(a) - sizeB
+    for j in range(sizeB):
+        i = sizeA - 1 # start at last element
+        while i >= 0 and b[j] < a[i]:
+            a[i], a[i + 1] = b[j], a[i] # swap
+            i -= 1
+    return a
+
+# Test
+print merge2(A,B)
