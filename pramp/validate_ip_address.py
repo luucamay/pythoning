9aaacdfb8f2d8321fe3cf1f21358f8f53027ba24
luucamay <lupemaydana@gmail.com>
2019-10-26 19:36:23 -0400

    Description of the problem Validate IP Adress

--- pramp/validate_ip_address.py
+++ pramp/validate_ip_address.py
@@ -1,3 +1,14 @@
+'''
+Validate IP Address
+
+Validate an IP address (IPv4).
+An address is valid if and only if it is in the form
+"X.X.X.X", where each X is a number from 0 to 255.
+
+For example, "12.34.5.6", "0.23.25.0", and "255.255.255.255"
+are valid IP addresses, while "12.34.56.oops", "1.2.3.4.5",
+and "123.235.153.425" are invalid IP addresses.
+'''
 def validateIP(ip):
   array = ip.split('.')
   if len(array) == 4:
