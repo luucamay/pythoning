7a1b2de8754c994af3b577cca653472ec4c77e71
luucamay <lupemaydana@gmail.com>
2019-10-27 17:54:27 -0400

    Adding description to the problem

--- leetcode-daily_interview_problem/most_frequent_sum.py
+++ leetcode-daily_interview_problem/most_frequent_sum.py
@@ -1,3 +1,18 @@
+'''
+Most Frequent Subtree Sum
+
+Given a binary tree, find the most frequent subtree sum.
+
+Example:
+
+   3
+  / \
+ 1   -3
+
+The above tree has 3 subtrees. The root node with 3, and the 2 leaf nodes, which gives us a total of 3 subtree sums. The root node has a sum of 1 (3 + 1 + -3), the left leaf node has a sum of 1, and the right leaf node has a sum of -3. Therefore the most frequent subtree sum is 1.
+
+If there is a tie between the most frequent sum, you can return any one of them.
+'''
 from collections import defaultdict
 class Node():
     def __init__(self, value, left=None, right=None):
