eeb438df3f2b7d0a5c910a56764543e027887a59
luucamay <lupemaydana@gmail.com>
2019-10-30 00:02:04 -0400

    Adding description to the problem

--- leetcode-daily_interview_problem/task_scheduler.py
+++ leetcode-daily_interview_problem/task_scheduler.py
@@ -1,3 +1,19 @@
+'''
+Task scheduler (counting slots required)
+
+We have a list of tasks to perform, with a cooldown period. We can do multiple of these at the same time,
+but we cannot run the same task simultaneously.
+
+Given a list of tasks, find how long it will take to complete the tasks in the order they are input.
+
+Exapmle:
+tasks = [1, 1, 2, 1]
+cooldown = 2
+output: 7 (order is 1 _ _ 1 2 _ 1)
+
+Solution translated from Java to Python from:
+https://sugarac.gitbooks.io/facebook-interview-handbook/task-scheduler.html
+'''
 from collections import defaultdict
 def task_schedule(tasks, cooldown):
     if tasks == None or len(tasks) == 0:
