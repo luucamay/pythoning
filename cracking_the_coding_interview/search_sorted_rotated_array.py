b226d4a0e637c2c01afef1c494ede25bd3f0b630
luucamay <lupemaydana@gmail.com>
2019-10-31 17:38:14 -0400

    Adding description

--- cracking_the_coding_interview/search_sorted_rotated_array.py
+++ cracking_the_coding_interview/search_sorted_rotated_array.py
@@ -1,3 +1,19 @@
+'''
+Search in Rotated Array:
+
+Given a sorted array of n integers that has been rotated an unknown
+number of times, write code to find an element in the array.
+You may assume that the array was originally sorted in increasing order.
+
+EXAMPLE
+input: find 5 in {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
+output: 8 (the index of 5 in the array) 
+
+Hints
+# 298: Can you modify Binary Search for this? 
+# 310: What if the array has duplicates?
+'''
+# Solution works for 193/196 cases at leetcode
 def search_sorted_rotated(arr, key):
     return search(arr, 0, len(arr) - 1, key)
 
