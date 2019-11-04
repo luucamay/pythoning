026f78ab8ccb8402362a49804a7d559a442b8b64
luucamay <lupemaydana@gmail.com>
2019-11-04 11:15:12 -0400

    Added description to the problem

--- leetcode-daily_interview_problem/list_cousins.py
+++ leetcode-daily_interview_problem/list_cousins.py
@@ -1,5 +1,5 @@
 '''
-List cousins
+List cousins - Daily Interview Problem
 Given a binary tree and a given node value, return all of the node's cousins. 
 Two nodes are considered cousins if they are on the same level of the tree with different parents. 
 You can assume that all nodes will have their own unique value.
