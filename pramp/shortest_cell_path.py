c9ba944608688eeb9e56d6b7af1533ae636b45d5
luucamay <lupemaydana@gmail.com>
2019-11-06 10:41:53 -0400

    Improviming solution adding a for loop so we do not repeat code

---
+++ pramp/shortest_cell_path.py
@@ -0,0 +1,68 @@
+'''
+Shortest cell path - Pramp
+
+In a given grid of 0s and 1s, we have some starting row and column sr, sc and
+a target row and column tr, tc. Return the length of the shortest path
+from sr, sc to tr, tc that walks along 1 values only.
+
+Each location in the path, including the start and the end, must be a 1.
+Each subsequent location in the path must be 4-directionally adjacent to the previous location.
+
+It is guaranteed that grid[sr][sc] = grid[tr][tc] = 1,
+and the starting and target positions are different.
+
+If the task is impossible, return -1.
+
+Examples:
+
+input:
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1]]
+sr = 0, sc = 0, tr = 2, tc = 0
+output: 8
+(The lines below represent this grid:)
+1111
+0001
+1111
+
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 0, 1, 1]]
+sr = 0, sc = 0, tr = 2, tc = 0
+output: -1
+(The lines below represent this grid:)
+1111
+0001
+1011
+'''
+def shortestCellPath(grid, sr, sc, tr, tc): 
+  num_col = len(grid[0])
+  num_row = len(grid)
+  visited = set()
+  current = [(sr, sc)]
+  path_distance = {}
+  path_distance[(sr, sc)] = 0
+  while current: 
+    r, c = current.pop(0)
+    d = path_distance[(r, c)]
+    if r == tr and c == tc:
+      return d
+    for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+        if r >= 0 and r < num_row and c >= 0 and c < num_col: # not out of the grid
+            if grid[r][c] == 1 and (r, c) not in visited:
+                current.append((nr, nc))
+                path_distance[(nr, nc)] = d + 1
+    visited.add((r, c))
+  return -1
+
+# Test cases:
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 0, 1, 1]]
+sr = 0
+sc = 0
+tr = 2
+tc = 0
+print shortestCellPath(grid, sr, sc, tr, tc)
+
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1]]
+sr = 0
+sc = 0
+tr = 2
+tc = 0
+print shortestCellPath(grid, sr, sc, tr, tc)
