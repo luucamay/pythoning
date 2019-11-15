70dbc812b66fa68b129b1c6484dff17bf7ef384f
luucamay <lupemaydana@gmail.com>
2019-11-15 12:59:24 -0400

    Description added

--- cracking_the_coding_interview/robot_in_a_grid.py
+++ cracking_the_coding_interview/robot_in_a_grid.py
@@ -1,3 +1,17 @@
+'''
+Robot in a Grid
+Cracking the coding interview, chapter 8, exercise #2
+
+Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+The robot can only move in two directions, right and down, but certain cells are "off limits" such that
+the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to
+the bottom right.
+Hints:
+#331: For the robot to reach the last cell, it must find a path to the second-to-last cells. For it to
+find a path to the second-to-last cells, it must find a path to the third-to-last cells.
+#360: Simplify this problem a bit by first figuring out if there's a path. Then, modify your algorithm to track the path.
+#388: Think again about the efficiency of your algorithm. Can you optimize it?
+'''
 def get_path(grid):
     if grid == None or len(grid) == 0:
         return []
