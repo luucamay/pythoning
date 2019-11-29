8ec7955596b5f1dc72b21ba42b90755c7bd6a6b1
luucamay <lupemaydana@gmail.com>
2019-11-29 16:27:23 -0400

    Added a test case

--- pramp/sentence_reverse_words.py
+++ pramp/sentence_reverse_words.py
@@ -32,3 +32,7 @@ def reverse_words(arr):
       spaces = []
   output = spaces + word + output
   return output
+
+# Test
+arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
+print reverse_words(arr)
