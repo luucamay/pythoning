3ed6e1a5ea049fdca8fa519e2fc538597c8f1a3b
luucamay <lupemaydana@gmail.com>
2020-02-13 15:59:28 -0400

    Solution to petya strings

--- codeforces/petya_strings.py
+++ codeforces/petya_strings.py
@@ -2,4 +2,13 @@
 Petya and Strings - Codeforces
 https://codeforces.com/problemset/problem/112/A
 '''
-
+words = [None]*2
+words[0] = input().lower()
+words[1] = input().lower()
+new_arr = sorted(words)
+if words[0] == words[1]:
+    print(0)
+elif words[0] == new_arr[0]:
+    print(-1)
+else:
+    print(1)
