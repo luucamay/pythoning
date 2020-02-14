1a7aa6c0af1565c56dc067c4f634bee962b87d26
luucamay <lupemaydana@gmail.com>
2020-02-14 10:59:45 -0400

    New solution created for is unique problem

--- cracking_the_coding_interview/is_unique.py
+++ cracking_the_coding_interview/is_unique.py
@@ -1,12 +1,20 @@
 '''
+1.1 Is Unique - Cracking the coding interview, P. 101
+
 Implement is_unique function that return True when all the characters in a string are different.
+What if you cannot use additional data structures?
+
+Hints
+44: Try a hash table
+84: There is one solution that is O(N log N) time. Another solution uses some space, but is O(N) time.
+132: Can you solve it in O(N log N) time? What might a solution look like?
 '''
 # First solution
 def is_unique1(string):
     n = len (string)
-    new_string = sorted(string)
+    list_string = sorted(string)
     for i in range (n - 1):
-        if new_string[i] == new_string[i +1]:
+        if list_string[i] == list_string[i +1]:
             return False
     return True
 '''
@@ -22,6 +30,16 @@ def is_unique2(string):
         if (letters[val]):
             return False
         letters[val] = 1
+    return True
+
+# Third solution - using data structure set
+def is_unique3(string):
+    set_string = set()
+    for c in string:
+        if c in set_string:
+            return False
+        else:
+            set_string.add(c)
     return True
 
 # Test
